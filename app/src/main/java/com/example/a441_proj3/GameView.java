0d3eaf52628052504772b494c28c2ca1a602c19e
mlee138 <mlee138@binghamton.edu>
2019-02-27 17:52:28 -0500

    Added touch functionality and a falling orb

--- app/src/main/java/com/example/a441_proj3/GameView.java
+++ app/src/main/java/com/example/a441_proj3/GameView.java
@@ -11,6 +11,7 @@ import android.graphics.Rect;
 import android.os.Handler;
 import android.util.Log;
 import android.view.Display;
+import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
@@ -30,6 +31,9 @@ public class GameView extends View {
 
     Bitmap[] orbs;
     int orbFrame = 0;
+    int velocity = 0;
+    int gravity = 3;
+    int orbX, orbY;
 
     public GameView(Context context){
         super(context);
@@ -48,9 +52,11 @@ public class GameView extends View {
         dWidth = point.x;
         dHeight = point.y;
         rect = new Rect(0,0,dWidth,dHeight);
-        orbs = new Bitmap[2];
+        orbs = new Bitmap[1];
         orbs[0] = BitmapFactory.decodeResource(getResources(), R.drawable.orb);
-        orbs[1] = BitmapFactory.decodeResource(getResources(), R.drawable.orb2);
+//        orbs[1] = BitmapFactory.decodeResource(getResources(), R.drawable.orb2);
+        orbX = dWidth/2 - orbs[0].getWidth()/2;
+        orbY = dHeight/2-orbs[0].getHeight()/2;
     }
 
     @Override
@@ -58,14 +64,31 @@ public class GameView extends View {
         super.onDraw(canvas);
         //canvas.drawBitmap(background,0,0,null);
         canvas.drawBitmap(background,null,rect,null);
-        if(orbFrame == 0){
-            orbFrame = 1;
-        } else {
-            orbFrame = 0;
+//        if(orbFrame == 0){
+//            orbFrame = 1;
+//        } else {
+//            orbFrame = 0;
+//        }
+
+        if(orbY < dHeight - orbs[0].getHeight() || velocity < 0){
+            velocity += gravity;
+            orbY += velocity;
         }
 
-        canvas.drawBitmap(orbs[orbFrame],dWidth/2 - orbs[0].getWidth(), dHeight/2-orbs[0].getHeight()/2,null);
+
+
+        canvas.drawBitmap(orbs[orbFrame],orbX, orbY,null);
         handler.postDelayed(runnable,UPDATE_MS);
+    }
+
+
+    @Override
+    public boolean onTouchEvent (MotionEvent event){
+        int action = event.getAction();
+        if(action == MotionEvent.ACTION_DOWN){
+            velocity = -30;
+        }
+        return true;
     }
 
     /*void initView(){
