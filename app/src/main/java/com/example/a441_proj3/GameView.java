f14795998f5537b32d3c5960c915c34203645d1f
mlee138 <mlee138@binghamton.edu>
2019-02-27 01:59:38 -0500

    Added New Classes and debugged errors.

---
+++ app/src/main/java/com/example/a441_proj3/GameView.java
@@ -0,0 +1,36 @@
+package com.example.a441_proj3;
+
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.os.Handler;
+import android.view.View;
+
+public class GameView extends View {
+
+    Handler handler;
+    Runnable runnable;
+    final int UPDATE_VALUE = 30;
+    Bitmap background;
+
+    public GameView(Context context){
+        super(context);
+        handler = new Handler();
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                invalidate();
+            }
+        };
+        background = BitmapFactory.decodeResource(getResources(),R.drawable.background);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.drawBitmap(background,0,0,null);
+        handler.postDelayed(runnable,UPDATE_VALUE);
+    }
+}
--- app/src/main/java/com/example/a441_proj3/MainActivity.java
+++ app/src/main/java/com/example/a441_proj3/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.a441_proj3;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void startGame(View view){
+        Intent intent = new Intent(this, StartGame.class);
+        startActivity(intent);
+        finish();
     }
 }
